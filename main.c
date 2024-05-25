2da2f31271c5d016e1e73c7e6f2024c653fab8e2
Marcelo Belotto <marcelopdr@yahoo.com.br>
2024-05-24 22:28:10 -0300

    Adicionado variaveis de controle

--- main.c
+++ main.c
@@ -4,5 +4,28 @@
 
 int main()
 {
+    bool menuAtivado = true;
+    int opcaoSelecionada;
 
+    while (menuAtivado){
+        scanf("%d",&opcaoSelecionada);
+        switch(opcaoSelecionada){
+            case 1:
+            break;
+            case 2:
+            break;
+            case 3:
+            break;
+            case 4:
+            break;
+            case 5:
+            break;
+            case 6:
+                menuAtivado = false;
+            break;
+            default:
+            break;
+
+        }
+    }
 }
