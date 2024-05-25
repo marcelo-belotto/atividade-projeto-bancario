8667f33b3f90710cb81a40857897fd85a2c1e272
Marcelo Belotto <marcelopdr@yahoo.com.br>
2024-05-24 22:43:30 -0300

    Criação do Menu

--- main.c
+++ main.c
@@ -2,12 +2,35 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+void menu(){
+    for (int i = 0; i < 50; i++){
+        printf("=");
+    }
+    printf("\n");
+    printf("1 - CADASTRO\n");
+    printf("2 - DEPOSITAR\n");
+    printf("3 - SACAR\n");
+    printf("4 - TRANFERIR\n");
+    printf("5 - PIX\n");
+    printf("6 - CONSULTAR SALDO\n");
+    printf("7 - SAIR\n");
+    for (int i = 0; i < 50; i++){
+        printf("=");
+    }
+    printf("\n");
+}
+/*sacar, depositar, efetuar pix, cadastrar novo cliente, transferir dinheiro
+entre contas e consultar saldo. (A consulta deverá apresentar informação da conta de um
+cliente específico ou de todas as contas cadastradas).*/
+
 int main()
 {
     bool menuAtivado = true;
     int opcaoSelecionada;
 
     while (menuAtivado){
+        menu();
+        printf("OPERAÇÃO: ");
         scanf("%d",&opcaoSelecionada);
         switch(opcaoSelecionada){
             case 1:
@@ -21,6 +44,8 @@ int main()
             case 5:
             break;
             case 6:
+            break;
+            case 7:
                 menuAtivado = false;
             break;
             default:
